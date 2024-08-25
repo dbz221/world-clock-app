8a4fc1c174bd1eae64b885e4ff557c5e2495eea2
dbz221 <157700441+dbz221@users.noreply.github.com>
2024-08-25 23:28:24 +0330

    Added js functionalities
    including select city to display time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 function selectCountry(event) {
   let citySelected = event.target.value;
+
   if (citySelected.length > 0) {
+    if (citySelected === "current") {
+      citySelected = moment.tz.guess();
+    }
+    let cityString = citySelected.replace("_", " ").split("/")[1];
+    let cityContainer = document.querySelector(".time-container");
+    cityContainer.innerHTML = `<div class="city-container">
+          <div class="time" ">${moment
+            .tz(citySelected)
+            .format("hh:mm:ss [<small>]A[</small>]")}</div>
+          <div class="city">${cityString}</div>
+          <div class="date">${moment
+            .tz(citySelected)
+            .format("MMMM Do YYYY")}</div>
+        </div>`;
   }
 }
 function updateTime() {
@@ -8,6 +23,8 @@ function updateTime() {
   let londonDate = document.querySelector("#london-date");
   let tehranTime = document.querySelector("#tehran-time");
   let tehranDate = document.querySelector("#tehran-date");
+  let romeTime = document.querySelector("#milan-time");
+  let romeDate = document.querySelector("#milan-date");
 
   londonTime.innerHTML = moment
     .tz("Europe/London")
@@ -18,6 +35,11 @@ function updateTime() {
     .tz("Asia/Tehran")
     .format("hh:mm:ss [<small>]A[</small>]");
   tehranDate.innerHTML = moment.tz("Asia/Tehran").format("MMMM Do YYYY");
+
+  romeTime.innerHTML = moment
+    .tz("Europe/Rome")
+    .format("hh:mm:ss [<small>]A[</small>]");
+  romeDate.innerHTML = moment.tz("Europe/Rome").format("MMMM Do YYYY");
 }
 
 setInterval(updateTime, 1000);
